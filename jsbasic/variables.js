9915e3e459ec960bfa82ea0d84070352fa798b13
AbhiishekBh <157466889+AbhiishekBh@users.noreply.github.com>
2024-05-12 04:37:30 +0000

    variables

---
+++ jsbasic/variables.js
@@ -0,0 +1,26 @@
+const numbert = 234; // cant be changed
+
+// let and var can be change 
+// var dont know its scope and make big problem in js in past
+// then developer create let to solve this problem..
+
+// forget var and always use let or const...
+// dont use var not preffered create issue in block scope 
+// and function issue.....
+
+
+
+let email = "abhi343@gmail.com"
+var password = "a245"
+let home; // its value is undefined...
+
+console.log(email)
+console.log(password)
+console.log(numbert)
+
+//or
+
+console.table([email, password, numbert, home])
+
+
+
