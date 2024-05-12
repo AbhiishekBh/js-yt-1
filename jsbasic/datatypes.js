76479b53e35b82654a476b747a5cef2e101aa5eb
AbhiishekBh <157466889+AbhiishekBh@users.noreply.github.com>
2024-05-12 05:00:10 +0000

    datatypes

---
+++ jsbasic/datatypes.js
@@ -0,0 +1,29 @@
+// in previous js there is no array or anything but then it changed
+
+"use strict"; //treat all JS code as newer version of js
+
+// alert() dont use directly in node anotherwise you will get error
+// in browser you can use it directly
+// use can use some extension like (prettier) to enhance your code readibility
+ 
+// MDN documentation or tc-39.es originall documentation
+
+let name = "abhisek" // string datatype
+let age = 21 // number datatype
+let isTrue = true // boolean type
+console.table([name, age,isTrue ])
+
+// number=> 2 to 53
+// bigint
+// boolean =>true/false
+// string =>""
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+//  typeof => use to find the type of a variable..
+
+console.log(typeof(age));
+
