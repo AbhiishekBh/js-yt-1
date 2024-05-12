7cb9ba225d6b1b0cfceb5e5712cee17f08ff6f33
AbhiishekBh <157466889+AbhiishekBh@users.noreply.github.com>
2024-05-12 08:46:19 +0000

    more about cndc

--- jsbasic/cndc.js
+++ jsbasic/cndc.js
@@ -1,14 +1,15 @@
 // conversion confusion
-console.log(45)
+
+//console.log(45)
 
 let score = "55abc"
- console.log(typeof score)
- console.log(typeof(score))
+ //console.log(typeof score)
+ //console.log(typeof(score))
 
  let numberValue = Number(score)// converting datatype if score into number
 
- console.log(typeof numberValue)
- console.log(numberValue)
+ //console.log(typeof numberValue)
+ //console.log(numberValue)
 
  // "33" => 33
  // "33anc" => NaN
@@ -17,7 +18,23 @@ let score = "55abc"
  // etc....
  let someNumber = 45
  let stringNumber= String(someNumber)
- console.log(stringNumber)
- console.log(typeof stringNumber)
+ //console.log(stringNumber)
+ //console.log(typeof stringNumber)
+
+ // *********** operation ******
+
+ let value = 8
+ let negValue = -value
+ //console.log(negValue)//-8
+
+ // console.log(2+2)
+ // console.log(2*2)   etc...
+
+ console.log(+true) // 1
+ //console.log(true+)// error
+ // console.log(+"") // 0
+
+ // learn about prefix and postfix opertion loke ++num ,, num++
+
 
 
